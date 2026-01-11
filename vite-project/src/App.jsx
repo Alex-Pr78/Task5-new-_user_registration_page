1e199bcc0dc71a00cfbed5b209c0755418a42099
Алексей <METEOP2024@yandex.ru>
2026-01-11 21:39:13 +0300

    added states

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,34 @@
-// import { useState } from 'react'
-
+import { useState } from "react";
 import styles from "./App.module.css";
 
+const sendData = (formData) => {
+  console.log(formData)
+};
+
 export const App = () => {
-  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    sendData({ email, password, confirmPassword });
+  };
+
   return (
     <>
       <div className={styles.container}>
-        <h2>Регистрация нового пользователя</h2>
-        <form className={styles.form} noValidate>
+        <h2>Регистрация</h2>
+        <form className={styles.form} onSubmit={onSubmit}>
           <label className={styles.label}>
             Email:
             <input
               type="email"
               name="email"
+              value={email}
               className={styles.input}
+              onChange={({ target }) => setEmail(target.value)}
             />
             {<p className={styles.error}></p>}
           </label>
@@ -24,7 +38,9 @@ export const App = () => {
             <input
               type="password"
               name="password"
+              value={password}
               className={styles.input}
+              onChange={({ target }) => setPassword(target.value)}
             />
             {<p className={styles.error}></p>}
           </label>
@@ -34,13 +50,16 @@ export const App = () => {
             <input
               type="password"
               name="confirmPassword"
+              value={confirmPassword}
               className={styles.input}
+              onChange={({ target }) => setConfirmPassword(target.value)}
             />
             {<p className={styles.error}></p>}
           </label>
 
-          <button type="submit" className={styles.button}>Зарегистрироваться</button>
-          
+          <button type="submit" className={styles.button}>
+            Зарегистрироваться
+          </button>
         </form>
       </div>
     </>
