67c75dcc972aa9a1090a070a219f1ed23702e681
Алексей <METEOP2024@yandex.ru>
2026-01-11 20:07:45 +0300

    project start

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import styles from './App.module.css'
+
+export const App = () => {
+
+  return (
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Hello</h1>
+      </div>
+    </>
+  )
+}
+
+
