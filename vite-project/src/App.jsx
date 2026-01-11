4331aeed0149aa755e89b5a2fdc326990469caf9
Алексей <METEOP2024@yandex.ru>
2026-01-11 20:41:09 +0300

    added a form renderer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,48 @@
 // import { useState } from 'react'
 
-import styles from './App.module.css'
+import styles from "./App.module.css";
 
 export const App = () => {
-
+  
   return (
     <>
       <div className={styles.container}>
-        <h1 className={styles.title}>Hello</h1>
+        <h2>Регистрация нового пользователя</h2>
+        <form className={styles.form} noValidate>
+          <label className={styles.label}>
+            Email:
+            <input
+              type="email"
+              name="email"
+              className={styles.input}
+            />
+            {<p className={styles.error}></p>}
+          </label>
+
+          <label className={styles.label}>
+            Пароль:
+            <input
+              type="password"
+              name="password"
+              className={styles.input}
+            />
+            {<p className={styles.error}></p>}
+          </label>
+
+          <label className={styles.label}>
+            Повторите пароль:
+            <input
+              type="password"
+              name="confirmPassword"
+              className={styles.input}
+            />
+            {<p className={styles.error}></p>}
+          </label>
+
+          <button type="submit" className={styles.button}>Зарегистрироваться</button>
+          
+        </form>
       </div>
     </>
-  )
-}
-
-
+  );
+};
